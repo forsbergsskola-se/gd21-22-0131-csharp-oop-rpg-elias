17a317ff490d3302d5e51e994bfcaa800d53c234
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-01-31 15:05:23 +0100

    Updated array

--- projects/assignments-003.3.5-console.basics-4/P16Arrays/P16Arrays/Program.cs
+++ projects/assignments-003.3.5-console.basics-4/P16Arrays/P16Arrays/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int[] array = new int[11];
+/*array[0] = 0;
+array[1] = 1;
+array[2] = 2;
+array[3] = 3;
+array[4] = 4;
+array[5] = 5;
+array[6] = 6;
+array[7] = 7;
+array[8] = 8;
+array[9] = 9;
+array[10] = 10;*/
 
-Console.WriteLine("Hello, World!");
+
+foreach (var Value in array)
+{
+    Console.WriteLine(Value);
+}

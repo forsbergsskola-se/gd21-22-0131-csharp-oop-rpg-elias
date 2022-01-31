d25966902c359da14235bda9e4d52ada244d6a23
SearWaizy <sear.waizy@student.forsbergsskola.se>
2022-01-31 14:37:03 +0100

    P14 ForLoops (Not Finished)

--- projects/assignments-003.3.5-console.basics-4/P14ForLoops/P14ForLoops/Program.cs
+++ projects/assignments-003.3.5-console.basics-4/P14ForLoops/P14ForLoops/Program.cs
@@ -1,5 +1,15 @@
 ﻿
-for (int counter1 = 1; counter1 < 1000; counter1++)
+for (int counter1 = 1; counter1 <= 1000; counter1++)
 {
     Console.WriteLine(counter1);
+}
+
+for (int counter2 = 200; counter2 >= 100; counter2--)
+{
+    Console.WriteLine(counter2);
+}
+
+for (int counter3 = 1024; counter3 >= 1; counter3 )
+{
+    Console.WriteLine(counter3);
 }

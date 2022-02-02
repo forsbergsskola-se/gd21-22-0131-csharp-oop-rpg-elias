203f40b7b68bde72c34a055cc1a43a0cd0e27597
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-02-02 13:48:32 +0100

    Parameters 1

--- projects/Assignments-003.4-Console-Methods/P1Methods/P1Methods/Program.cs
+++ projects/Assignments-003.4-Console-Methods/P1Methods/P1Methods/Program.cs
@@ -1,21 +1,23 @@
-﻿CountUp();
-CountDown();
-CountUp();
-CountDown();
+﻿CountUp(1);
+CountDown(1);
+CountUp(2);
+CountDown(2);
+CountUp(3);
+CountDown(3);
 
-void CountUp()
+void CountUp(int to)
 {
-    Console.WriteLine("Counting from 0 to 5:");
-    for (int number = 0; number <= 5; number++)
+    Console.WriteLine("Counting from 0 to " + to + ":" );
+    for (int number = 0; number <= to; number++)
     {
         Console.WriteLine(number);
     }
 }
 
-void CountDown()
+void CountDown(int from)
 {
-    Console.WriteLine("Counting from 5 to 0:");
-    for (int number = 5; number >= 0; number--)
+    Console.WriteLine("Counting from " + from +" to 0:" );
+    for (int number = from; number >= 0; number--)
     {
         Console.WriteLine(number);
     }

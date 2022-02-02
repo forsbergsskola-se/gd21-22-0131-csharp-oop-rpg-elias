61b97c66455a2b3e9adb9f1ec2e54732c5486080
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-02-02 13:28:06 +0100

    P1Methods

--- projects/Assignments-003.4-Console-Methods/P1Methods/P1Methods/Program.cs
+++ projects/Assignments-003.4-Console-Methods/P1Methods/P1Methods/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿CountUp();
+CountDown();
+CountUp();
+CountDown();
 
-Console.WriteLine("Hello, World!");
+void CountUp()
+{
+    Console.WriteLine("Counting from 0 to 5:");
+    for (int number = 0; number <= 5; number++)
+    {
+        Console.WriteLine(number);
+    }
+}
+
+void CountDown()
+{
+    Console.WriteLine("Counting from 5 to 0:");
+    for (int number = 5; number >= 0; number--)
+    {
+        Console.WriteLine(number);
+    }
+}

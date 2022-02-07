b441257adb777caf2e9bcf37e2447a6467ef713e
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-02-07 21:14:48 +0100

    Fixed p2

--- projects/Assignments-003.5.1-Console-Classes-1/P2Classes/P2Classes/Program.cs
+++ projects/Assignments-003.5.1-Console-Classes-1/P2Classes/P2Classes/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Person person1 = new Person();
+Animal animal1 = new Animal();
+Car car1 = new Car();
 
-Console.WriteLine("Hello, World!");
+person1.Head();
+animal1.Paw();
+car1.Tires();
+
+class Person
+{
+    public void Head()
+    {
+        Console.WriteLine("Person");
+    }
+}
+
+class Animal
+{
+    public void Paw()
+    {
+        Console.WriteLine("Animal");
+    }
+}
+
+class Car
+{
+    public void Tires()
+    {
+        Console.WriteLine("Car");
+    }
+}

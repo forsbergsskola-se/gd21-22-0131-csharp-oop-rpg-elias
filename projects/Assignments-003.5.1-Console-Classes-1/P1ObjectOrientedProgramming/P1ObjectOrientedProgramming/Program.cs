ada854e8b853b870241bee85f90bdc84aa1d8944
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-02-07 14:21:52 +0100

    P1 exercise done!

---
+++ projects/Assignments-003.5.1-Console-Classes-1/P1ObjectOrientedProgramming/P1ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿    string[] names = new string[3];
+    for (int i = 0; i < names.Length; i++)
+    {
+        Console.WriteLine("Give me a name.");
+        if (i == 0)
+        {
+            names[0] = Console.ReadLine();
+        }
+        else if (i == 1)
+        {
+            names[1] = Console.ReadLine();
+        }
+        else if (i == 2)
+        {
+            names[2] = Console.ReadLine();
+        }
+    }
+    Console.WriteLine(names[0] + "," + names[1] + "," + names[2]);
